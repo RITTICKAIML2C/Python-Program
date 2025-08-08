a792750ebb546938bee19d345e03edbff0172d6c
RITTICK RAJ MISHRA <rittickrajmishra@gmail.com>
2025-08-08 09:42:36 +0530

    Add files via upload

---
+++ Temperature Scale Conversion..py
@@ -0,0 +1,7 @@
+c = float(input("Enter temperature in Celsius: "))
+f = (c * 9/5) + 32
+print("In Fahrenheit:", f)
+
+f = float(input("Enter temperature in Fahrenheit: "))
+c = (f - 32) * 5/9
+print("In Celsius:", c)
