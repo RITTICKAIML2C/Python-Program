f21f3fec6aaa8a2a5edd40816af83a1f51e539ab
RITTICK RAJ MISHRA <rittickrajmishra@gmail.com>
2025-08-08 09:33:01 +0530

    Add files via upload

---
+++ Simple Interest.py
@@ -0,0 +1,5 @@
+principal = float(input("Enter the principal amount: "))
+rate = float(input("Enter the rate of interest: "))
+time = float(input("Enter time of interest: "))
+simple_interest = (principal * rate * time) / 100
+print("The required Simple Interest is: ", simple_interest)
